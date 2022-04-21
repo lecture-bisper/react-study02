5a622081eb348815acd3bb7dbbebed96bc683889
lecture-bisper <bisper@outlook.kr>
2022-04-21 09:35:05 +0900

    jsx 문법에서 html 사용시 주의점.

--- src/Address.js
+++ src/Address.js
@@ -1,6 +1,8 @@
 import React from "react";
 import "./Address.css";
 
+// jsx 문법에서는 html 태그를 사용 시 반드시 시작태그와 끝태그가 존재해야함
+// <img> -> <img></img>, <img />, <br> -> <br></br>, <br />, <hr> -> <hr></hr>, <hr />
 function Addr(props) {
   const { name, gender, email } = props;
 
