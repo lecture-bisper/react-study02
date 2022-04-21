9e737d4197c8d5ebf40a2ed9cc3f0cecaf701ecf
lecture-bisper <bisper@outlook.kr>
2022-04-21 16:39:16 +0900

    컴포넌트의 생명주기를 사용하는 문제.

--- src/index.js
+++ src/index.js
@@ -1,14 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+// import App from './App';
+// import App2 from './App2';
+// import App3 from './App3';
+// import App4 from './App4';
+// import App5 from './App5';
+import App6 from './App6';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    {/* <App /> */}
+    {/* <App2 /> */}
+    {/* <App3 /> */}
+    {/* <App4 /> */}
+    {/* <App5 /> */}
+    <App6 />
+  </React.StrictMode>,
 );
 
 // If you want to start measuring performance in your app, pass a function
