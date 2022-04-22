97632ca8b43a244f208a2b3116300b4ad05365b7
lecture-bisper <bisper@outlook.kr>
2022-04-22 16:54:14 +0900

    HashRouter 대신 Routes 사용하기 위하여 수정.

--- src/App8.js
+++ src/App8.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { HashRouter, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import About from './routes/About';
 import Home from './routes/Home';
 import Detail from './routes/Detail';
@@ -7,12 +7,14 @@ import Navigation from './components/Navigation';
 
 function App8() {
   return (
-    <HashRouter>
+    <div>
       <Navigation />
-      <Route path="/" component={Home} exact={true} />
-      <Route path="/about" component={About} />
-      <Route path="/movie-detail" component={Detail} />
-    </HashRouter>
+      <Routes>
+        <Route path="/" element={<Home />} exact={true} />
+        <Route path="/about" element={<About />} />
+        <Route path="/movie-detail" element={<Detail />} />
+      </Routes>
+    </div>
   );
 }
 
