d4e0ae6d839d9c6a7215aa7d6f94c9415e00dc47
lecture-bisper <bisper@outlook.kr>
2022-04-22 16:10:30 +0900

    1. 새로운 App 컴포넌트 App8 생성 2. routes, components 폴더 생성 및 파일 이동.

---
+++ src/App8.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { HashRouter, Route } from 'react-router-dom';
+import About from './routes/About';
+import Home from './routes/Home';
+import Detail from './routes/Detail';
+import Navigation from './components/Navigation';
+
+function App8() {
+  return (
+    <HashRouter>
+      <Navigation />
+      <Route path="/" component={Home} exact={true} />
+      <Route path="/about" component={About} />
+      <Route path="/movie-detail" component={Detail} />
+    </HashRouter>
+  );
+}
+
+export default App8;
--- src/index.js
+++ src/index.js
@@ -1,27 +1,45 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+import { BrowserRouter } from 'react-router-dom';
 // import App from './App';
 // import App2 from './App2';
 // import App3 from './App3';
 // import App4 from './App4';
 // import App5 from './App5';
 // import App6 from './App6';
-import App7 from './App7';
+// import App7 from './App7';
+import App8 from './App8';
 import reportWebVitals from './reportWebVitals';
+
+import './index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    {/* <App /> */}
-    {/* <App2 /> */}
-    {/* <App3 /> */}
-    {/* <App4 /> */}
-    {/* <App5 /> */}
-    {/* <App6 /> */}
-    <App7 />
-  </React.StrictMode>,
+  <BrowserRouter>
+    <App8 />
+  </BrowserRouter>,
 );
+
+// ReactDOM.render(
+//   <BrowserRouter>
+//     <App8 />
+//   </BrowserRouter>,
+//   document.getElementById('root'),
+// );
+
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(
+//   <React.StrictMode>
+//     {/* <App /> */}
+//     {/* <App2 /> */}
+//     {/* <App3 /> */}
+//     {/* <App4 /> */}
+//     {/* <App5 /> */}
+//     {/* <App6 /> */}
+//     {/* <App7 /> */}
+//     <App8 />
+//   </React.StrictMode>,
+// );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
