6001b99d9faa804d65ca6b2e488afa25674e0c9d
lecture-bisper <bisper@outlook.kr>
2022-04-22 16:53:43 +0900

    Movie 컴포넌트에 Detail 컴포넌트로 이동하기 위한 Link 태그 추가.

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,26 +1,29 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 
 import './Movie.css';
 
 function Movie({ year, title, summary, poster, genres }) {
   return (
     <div className="movie">
-      <img src={poster} alt={title} title={title} />
-      <div className="movie-data">
-        <h3 className="movie-title">{title}</h3>
-        <h5 className="movie-year">{year}</h5>
-        <ul className="movie-genres">
-          {genres.map((genre, index) => {
-            return (
-              <li key={index} className="movie-genre">
-                {genre}
-              </li>
-            );
-          })}
-        </ul>
-        <p className="movie-summary">{summary.slice(0, 180)}...</p>
-      </div>
+      <Link to={{ pathname: '/movie-detail', state: { year, title, summary, poster, genres } }}>
+        <img src={poster} alt={title} title={title} />
+        <div className="movie-data">
+          <h3 className="movie-title">{title}</h3>
+          <h5 className="movie-year">{year}</h5>
+          <ul className="movie-genres">
+            {genres.map((genre, index) => {
+              return (
+                <li key={index} className="movie-genre">
+                  {genre}
+                </li>
+              );
+            })}
+          </ul>
+          <p className="movie-summary">{summary.slice(0, 180)}...</p>
+        </div>
+      </Link>
     </div>
   );
 }
